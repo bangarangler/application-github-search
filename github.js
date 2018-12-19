d315016dbaa33f4e32637aaa53725db1b503063a
Jonathan Dain Palacio <bangarangler@gmail.com>
2018-12-19 16:47:17 -0500

    finished up project. added 5 per page of repos. added repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
 	constructor() {
 		this.client_id = 'd535e83a02e85f214688';
 		this.client_secret = 'f910d627fc21787175f71f66573897316bf7556f';
+		this.repos_count = 5;
+		this.repos_sort = 'created: asc';
 	}
 
 	async getUser(user) {
 		const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+		const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
 		const profile = await profileResponse.json();
+		const repos = await repoResponse.json();
 
 		return {
-			profile
+			profile,
+			repos
 		};
 	}
 }
